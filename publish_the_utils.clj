999ed4c7647b10107ccf491218eb4c788af97b72
kir <kirill-gerasimenko-da@users.noreply.github.com>
2023-01-20 14:02:24 +0100

    add script for publish

---
+++ publish_the_utils.clj
@@ -0,0 +1,21 @@
+#!/usr/bin/env bb
+
+(ns publish
+  (:require [babashka.process :refer [process check]]))
+
+(defn run [dir args]
+  (-> (process args {:dir dir :err :inherit :out :inherit})
+      (check)
+      :exit))
+
+(run "."
+  ["dotnet" "build"
+   "./src/TheUtils/TheUtils.csproj"
+   "-c" "Release"])
+
+(run "."
+  ["dotnet" "pack"
+   "./src/TheUtils/TheUtils.csproj"
+   "-c" "Release"
+   "-o" "./publish"
+   "/p:PackageVersion=0.0.1"])
