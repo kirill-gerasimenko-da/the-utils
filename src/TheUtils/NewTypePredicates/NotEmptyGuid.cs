0ed6dde65eb2481d2d2792c2b937346cc5b2482c
kir <kirill-gerasimenko-da@users.noreply.github.com>
2023-06-12 21:53:55 +0200

    remove not used often classes

--- src/TheUtils/NewTypePredicates/NotEmptyGuid.cs
+++
@@ -1,8 +0,0 @@
-namespace TheUtils.NewTypePredicates;
-
-using LanguageExt.TypeClasses;
-
-public struct NotEmptyGuid : Pred<Guid>
-{
-    public bool True(Guid value) => value != Guid.Empty;
-}
--- src/TheUtils/NewTypePredicates/NotEmptyString.cs
+++
@@ -1,8 +0,0 @@
-namespace TheUtils.NewTypePredicates;
-
-using LanguageExt.TypeClasses;
-
-public struct NotEmptyString : Pred<string>
-{
-    public bool True(string value) => !string.IsNullOrEmpty(value);
-}
