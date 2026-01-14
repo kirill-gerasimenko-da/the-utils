96df9dca0018c4f1a7fd97557e5c330c9caf16da
kir <kirill-gerasimenko-da@users.noreply.github.com>
2026-01-14 17:02:59 +0100

    remove old files

--- tests/TheUtils.Db.Postgresql.Tests/PostgresDbAdvisoryLockAdvancedTests.cs
+++
@@ -1,2 +0,0 @@
-// This file has been emptied due to connection pool issues.
-// Core advisory lock tests exist in PostgresDbTests.cs
--- tests/TheUtils.Db.Tests/SqliteFixture.cs
+++
@@ -1,2 +0,0 @@
-// This file is deprecated. See PostgreSqlFixture.cs instead.
-// The test project now uses PostgreSQL with Testcontainers for all tests.
